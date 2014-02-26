a66ad7f2cb5d25bb2a43797acb828715d51bc7ae
christianarg <christianarg@gmail.com>
2014-02-26 08:02:17 +0100

    Corregir EF4EF5TransactionScope
    Había subido mal el proyecto

---
+++ EF4EF5TransactionScope/EF4Ef5TransactionScopeTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
+using System.Transactions;
+using System.Data.EntityClient;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Como hacer que puedan convivir EF4 y EF5 dentro de una misma TransactionScope
+    /// sin hace que se promueva a transacción distribuida, por lo que por ejemplo,
+    /// no funcionaría en Azure
+    /// </summary>
+    [TestClass]
+    public class EF4Ef5TransactionScopeTest
+    {
+
+        [TestInitialize]
+        public void Init()
+        {
+            using (var ctx = new Model1Container1())
+            using (var dbCtx = new MyDbContext())
+            {
+                ctx.DeleteDatabase();
+                ctx.CreateDatabase();
+                //ctx.Database.Delete();
+                //ctx.Database.Create();
+                dbCtx.Database.Delete();
+                dbCtx.Database.Create();
+            }
+        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    using (var ctx = new Model1Container1())
+        //    {
+
+        //        ctx.Entity1Set.Add(new Entity1() { Id = 1, Property1 = "HolaMostro" });
+        //        ctx.SaveChanges();
+        //    }
+        //}
+
+        [TestMethod]
+        public void When_Share_Connection_Then_No_Distributed_Transaction_Promoted()
+        {
+            using (var tx = new TransactionScope())
+            using (var ctx = new Model1Container1())
+            using (var dbCtx = new MyDbContext((ctx.Connection as EntityConnection).StoreConnection))
+            //using (var dbCtx = new MyDbContext())
+            {
+               
+                
+                //ctx.Entity1Set.Add(new Entity1() { Id = 1, Property1 = "HolaMostro" });
+                ctx.Entity1Set.AddObject(new Entity1() { Id = 1, Property1 = "HolaMostro" });
+                
+                dbCtx.MyDbEntities.Add(new MyDbEntity() { Id = "1", TheProperty = "TheP" });
+
+                ctx.SaveChanges();
+                dbCtx.SaveChanges();
+                tx.Complete();
+            }
+        }
+    }
+}
---
+++ EF4EF5TransactionScope/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.EntityClient;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class MyDbEntity
+    {
+        [Key]
+        public string Id { get; set; }
+        public string TheProperty { get; set; }
+    }
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbConnection connection)
+            :base(connection,false)
+        {
+             
+        }
+        public MyDbContext() :base(){ }
+        //public MyDbContext(ObjectContext context)
+        //    : base(context,false)
+        //{
+        //}
+        public DbSet<MyDbEntity> MyDbEntities { get; set; }
+    }
+}
