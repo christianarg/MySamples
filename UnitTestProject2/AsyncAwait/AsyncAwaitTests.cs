9f4ebbc12cdeb4e1be2ef154823ad45bcdf33e37
christianarg <christianarg@gmail.com>
2014-11-26 10:41:31 +0100

    pruebas redis

---
+++ UnitTestProject2/AsyncAwait/AsyncAwaitTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace SamplesTestProyect.AsyncAwait
+{
+    [TestClass]
+    public class AsyncAwaitTests
+    {
+
+        [TestMethod]
+        public async Task UltraSimpleAsyncTest()
+        {
+            var data = await ReadStringAsync();
+            Assert.AreEqual(data, "Prueba");
+            Directory.GetCurrentDirectory();
+        }
+
+        private async Task<string> ReadStringAsync()
+        {
+            var task = Task.Factory.StartNew(() => "Prueba");
+
+            return task.Result;
+        }
+
+        [TestMethod]
+        public async Task TestReadFileAsync()
+        {
+            var data = await ReadFileAsync();
+            Assert.AreEqual(data, "Texto de ejemplo");
+            Directory.GetCurrentDirectory();
+        }
+
+        private async Task<string> ReadFileAsync()
+        {
+            using (var reader = new StreamReader("AsyncAwait\\ejemplo.txt"))
+            {
+                string data = await reader.ReadToEndAsync();
+                return data;
+            }
+        }
+
+
+       
+
+    }
+}
---
+++ UnitTestProject2/Redis/RedisTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace SamplesTestProyect.Redis
+{
+    [TestClass]
+    public class RedisTest
+    {
+        readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        IDatabase db;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            db = redis.GetDatabase();
+        }
+
+        [TestMethod]
+        public void HelloWorld()
+        {
+            db.StringSet("myKey", "Hello World");
+
+            var value = db.StringGet("myKey");
+            Assert.AreEqual("Hello World", value.ToString());
+        }
+
+        [TestMethod]
+        public void StoreObjectTest()
+        {
+            db.Set("myHash", new MyClass()
+            {
+                Id = "pepe",
+                MyValue = "MyValue"
+            });
+
+            var myClass = db.Get<MyClass>("myHash");
+
+            Assert.IsNotNull(myClass);
+            Assert.AreEqual("pepe",myClass.Id);
+            Assert.AreEqual("MyValue", myClass.MyValue);
+        }
+    }
+
+    public class MyClass
+    {
+        public string Id { get; set; }
+        public string MyValue { get; set; }
+    }
+
+    public static class RedisExtensions
+    {
+        public static T Get<T>(this IDatabase cache, string key)
+        {
+            return JsonConvert.DeserializeObject<T>(cache.StringGet(key));
+        }
+
+        public static object Get(this IDatabase cache, string key)
+        {
+            return JsonConvert.DeserializeObject<object>(cache.StringGet(key));
+        }
+
+        public static void Set(this IDatabase cache, string key, object value)
+        {
+            cache.StringSet(key, JsonConvert.SerializeObject(value));
+        }
+    }
+}
