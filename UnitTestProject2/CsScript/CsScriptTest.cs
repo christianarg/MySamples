affc45fffa15409aac2b8bcf7af1ef31383e9164
Christian Rodriguez <christianarg@gmail.com>
2015-01-07 11:01:03 +0100

    prueba simple csscript

---
+++ UnitTestProject2/CsScript/CsScriptTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSScriptLibrary;
+
+namespace PruebaCsScript
+{
+    [TestClass]
+    public class CsScriptTest
+    {
+        public TestContext TestContext { get; set; }
+        
+        [TestMethod]
+        public void LoadFileAndExecuteSimpleCode()
+        {
+
+            dynamic sumador = CSScript.Load("CsScript\\Sumador.scriptcs").CreateObject("*");
+            var suma = sumador.SumarOcho(8);
+            Assert.AreEqual(16,suma);
+        }
+    }
+}
