c15294eaf4717c9e37f7f5321556d6c22a3b97c5
Christian Rodriguez <christianarg@gmail.com>
2015-11-02 13:22:12 +0100

    pruebas impromptu

---
+++ ImpromptuInterfaceTests/ImpromptuInterfaceTests/ImpromptuTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Dynamic;
+using ImpromptuInterface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImpromptuInterfaceTests
+{
+    [TestClass]
+    public class ImpromptuTests
+    {
+        [TestMethod]
+        public void ExpandoForlerdosTest()
+        {
+            dynamic expando = new ExpandoObject();
+            expando.El = "Hola";
+            expando.Cosito = "Mostru";
+
+            Assert.AreEqual("Hola", expando.El);
+            Assert.AreEqual("Mostru", expando.Cosito);
+        }
+
+        [TestMethod]
+        public void ActLikeTest()
+        {
+            dynamic expando = new ExpandoObject();
+            //dynamic expando = Build<ExpandoObject>.NewObject(El: "Test", Cosito: "Mostru");
+            expando.El = "Hola";
+            expando.Cosito = "Mostru";
+
+            IMyCoso micoso = Impromptu.ActLike<IMyCoso>(expando);
+            Assert.AreEqual("Hola", micoso.El);
+            Assert.AreEqual("Mostru", micoso.Cosito);
+        }
+    }
+
+    public interface IMyCoso
+    {
+        string El { get; set; }
+        string Cosito { get; set; }
+    }
+
+
+}
