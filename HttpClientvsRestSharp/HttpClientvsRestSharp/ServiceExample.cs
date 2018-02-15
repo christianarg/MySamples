e5361c8d3d1a8189ff19c97b71e478781af7978f
Christian Rodriguez <christianarg@gmail.com>
2018-02-15 12:46:04 +0100

    pruebas resharp vs httpclient

---
+++ HttpClientvsRestSharp/HttpClientvsRestSharp/ServiceExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaHttpClient
+{
+    class ServiceExample
+    {
+        //private HttpClient httpClient;
+        //public HttpClient HttpClient
+        //{
+        //    get
+        //    {
+        //        if(httpClient == null)
+        //        {
+        //            httpClient = HttpClientFactory.CreateHttpClient(
+        //        }
+        //        return httpClient;
+        //    }
+        //}
+    }
+}
