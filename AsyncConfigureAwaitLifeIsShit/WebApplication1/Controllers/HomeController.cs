e7382b89f263bc4e65f386df17c384450580110a
Christian Rodriguez <christianarg@gmail.com>
2018-02-21 12:39:46 +0100

    Ejemplo de como debería ser async await y porque lamentablente hay que poner el puto ConfigureAwait(false) de los cojones en todos lados me cago en la puta

---
+++ AsyncConfigureAwaitLifeIsShit/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Index(string data)
+        {
+            ViewBag.data = data;
+            return View();
+        }
+
+
+        public ActionResult DeadLock()
+        {
+            var data = GetDataWithDefaultAwait().Result;
+            return RedirectToAction("Index", new { data = "got data from google" });
+        }
+
+      
+        public ActionResult NotDeadlock()
+        {
+            var data = GetDataWithAwaitFalse().Result;
+            return RedirectToAction("Index", new { data = "got data from google"});
+        }
+
+        public async Task<ActionResult> ActualCorrectWay()
+        {
+            var data = await GetDataWithDefaultAwait();
+            return RedirectToAction("Index", new { data = "got data from google" });
+        }
+
+        public async Task<string> GetDataWithDefaultAwait()
+        {
+            return await httpClient.GetStringAsync("https://www.google.es");
+        }
+
+        public async Task<string> GetDataWithAwaitFalse()
+        {
+            return await httpClient.GetStringAsync("https://www.google.es").ConfigureAwait(false);
+        }
+    }
+}
---
+++ AsyncConfigureAwaitLifeIsShit/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpClient httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    textBlock.Text = Coso().Result;
+        //}
+
+        private void Deadlock_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = GetDataWithDefaultAwait().Result;
+            //textBlock.Text = await Coso();
+        }
+
+        private async void UIThreadException_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = await GetDataWithDefaultAwait().ConfigureAwait(false);
+        }
+
+        private async void ActualCorrectWay_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = await GetDataWithDefaultAwait();
+        }
+
+        private async Task<string> GetDataWithDefaultAwait()
+        {
+            return await httpClient.GetStringAsync("https://www.google.es");
+        }
+    }
+}
