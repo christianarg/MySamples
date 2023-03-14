1c1ec2fee214f7ed9fcafca89fee842a01c9d4df
Christian Rodriguez <christianarg@gmail.com>
2023-03-14 08:31:25 +0100

    prueba generic host

---
+++ PruebaGenericHost/PruebaGenericHost/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PruebaGenericHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using var host = Host.CreateDefaultBuilder().ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<SampleHosted>();
+            }).Build();
+            host.Run();
+        }
+    }
+
+    public class SampleHosted : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("StartAsync");
+            await Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("StopAsync");
+            await Task.CompletedTask;
+        }
+    }
+}
